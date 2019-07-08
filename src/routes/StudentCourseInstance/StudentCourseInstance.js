97429e3cd6f49fff8c46cac62f23f47feb72f315
Paul McCloud <paul.mccloud@gmail.com>
2019-07-08 19:09:10 +0200

    add topic as header to StudentCourseInstance

--- src/routes/StudentCourseInstance/StudentCourseInstance.js
+++ src/routes/StudentCourseInstance/StudentCourseInstance.js
@@ -3,12 +3,23 @@ import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 import { Player, ControlBar, PlaybackRateMenuButton } from 'video-react';
 
-const StudentCourseInstance = ({ courseInstance: { notes, recapUrl } }) => {
+const StudentCourseInstance = ({
+    courseInstance: { notes, recapUrl, topic },
+}) => {
     return (
         <Paper>
             <Container maxWidth="md">
+                <Typography
+                    component="h2"
+                    variant="h6"
+                    color="primary"
+                    gutterBottom
+                >
+                    {topic}
+                </Typography>
                 {recapUrl ? (
                     <Player fluid src={recapUrl}>
                         <ControlBar>

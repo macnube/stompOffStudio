4f7d978ac990416259f07cbd00d86e31243077f2
Paul McCloud <paul.mccloud@gmail.com>
2020-01-07 18:59:25 +0100

    feat(CardDetail): Use hooks

--- src/routes/CardDetail/CardDetail.js
+++ src/routes/CardDetail/CardDetail.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import compose from 'recompose/compose';
 import find from 'lodash/find';
@@ -34,12 +34,31 @@ const columns = [
     },
 ];
 
-class CardDetail extends Component {
-    handleInstanceClick = rowData => this.navigateToCourseInstance(rowData[0]);
+const CardDetail = ({
+    card,
+    user,
+    classes,
+    markPrivateLessonUsed,
+    history,
+    updateCard,
+    removeCardParticipation,
+}) => {
+    const navigateToCourseInstance = id => {
+        history.push({
+            pathname: './courseInstance',
+            search: `id=${id}`,
+        });
+    };
 
-    handleUpdateCard = card => {
-        const { id, expirationDate, value, privateLessonLength } = card;
-        this.props.updateCard({
+    const handleInstanceClick = rowData => navigateToCourseInstance(rowData[0]);
+
+    const handleUpdateCard = ({
+        id,
+        expirationDate,
+        value,
+        privateLessonLength,
+    }) =>
+        updateCard({
             variables: {
                 id,
                 expirationDate,
@@ -47,89 +66,72 @@ class CardDetail extends Component {
                 privateLessonLength,
             },
         });
-    };
 
-    navigateToCourseInstance = id => {
-        this.props.history.push({
-            pathname: './courseInstance',
-            search: `id=${id}`,
-        });
-    };
-
-    navigateToStudentDetail = () => {
-        const { history, card } = this.props;
+    const navigateToStudentDetail = () =>
         history.push({
             pathname: './studentDetail',
             search: `id=${card.student.id}`,
         });
-    };
 
-    handleOnDeleteParticipationPress = id => {
-        const { removeCardParticipation, card } = this.props;
-
+    const handleOnDeleteParticipationPress = id =>
         removeCardParticipation({
             variables: {
                 id: card.id,
                 participantId: id,
             },
         });
-    };
 
-    renderParticipationHistorySelectedToolbar = (selectedRows, displayData) => (
-        <SelectedDeleteToolbar
-            selectedRows={selectedRows}
-            displayData={displayData}
-            handleOnDeletePress={this.handleOnDeleteParticipationPress}
-            getIds={this.getRowId}
-        />
-    );
-
-    getRowId = (selectedRows, displayData) => {
+    const getRowId = (selectedRows, displayData) => {
         const selectedDataIndex = selectedRows.data[0].dataIndex;
         return find(displayData, { dataIndex: selectedDataIndex }).data[0];
     };
 
-    render() {
-        const studentOptions = {
-            responsive: 'scroll',
-            selectedRows: 'single',
-        };
-        const adminOptions = {
-            ...studentOptions,
-            onRowClick: this.handleInstanceClick,
-            customToolbarSelect: this.renderParticipationHistorySelectedToolbar,
-        };
-        const { card, user, classes, markPrivateLessonUsed } = this.props;
-        return (
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <Container maxWidth="lg" className={classes.container}>
-                    <Grid container spacing={3}>
-                        <CardDetailHeader
-                            card={card}
-                            admin={user.admin}
-                            handleOnSave={this.handleUpdateCard}
-                            handleOnCancel={this.navigateToStudentDetail}
-                            handleOnMarkPrivateLessonUsed={
-                                this.handleMarkPrivateLessonUsed
-                            }
-                            markPrivateLessonUsed={markPrivateLessonUsed}
+    const renderParticipationHistorySelectedToolbar = (
+        selectedRows,
+        displayData
+    ) => (
+        <SelectedDeleteToolbar
+            selectedRows={selectedRows}
+            displayData={displayData}
+            handleOnDeletePress={handleOnDeleteParticipationPress}
+            getIds={getRowId}
+        />
+    );
+
+    const studentOptions = {
+        responsive: 'scroll',
+        selectedRows: 'single',
+    };
+    const adminOptions = {
+        ...studentOptions,
+        onRowClick: handleInstanceClick,
+        customToolbarSelect: renderParticipationHistorySelectedToolbar,
+    };
+
+    return (
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Container maxWidth="lg" className={classes.container}>
+                <Grid container spacing={3}>
+                    <CardDetailHeader
+                        card={card}
+                        admin={user.admin}
+                        handleOnSave={handleUpdateCard}
+                        handleOnCancel={navigateToStudentDetail}
+                        markPrivateLessonUsed={markPrivateLessonUsed}
+                    />
+                    <Grid item xs={12}>
+                        <MUIDataTable
+                            title={'Card Course History'}
+                            data={parseCardDataToArray(card)}
+                            columns={columns}
+                            options={user.admin ? adminOptions : studentOptions}
                         />
-                        <Grid item xs={12}>
-                            <MUIDataTable
-                                title={'Card Course History'}
-                                data={parseCardDataToArray(card)}
-                                columns={columns}
-                                options={
-                                    user.admin ? adminOptions : studentOptions
-                                }
-                            />
-                        </Grid>
                     </Grid>
-                </Container>
-            </MuiPickersUtilsProvider>
-        );
-    }
-}
+                </Grid>
+            </Container>
+        </MuiPickersUtilsProvider>
+    );
+};
 
 CardDetail.propTypes = {
     classes: PropTypes.object.isRequired,
--- src/routes/CardDetail/CardDetailHeader.js
+++ src/routes/CardDetail/CardDetailHeader.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import toNumber from 'lodash/toNumber';
 import TextField from '@material-ui/core/TextField';
@@ -14,238 +14,202 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import { DetailHeader } from 'components';
 import styles from './styles';
 
-class CardDetailHeader extends Component {
-    state = {
-        id: '',
-        expirationDate: new Date(),
-        value: 5,
-        canSave: false,
-        privateLessonLength: 0,
-        privateLessonUseDate: new Date(),
-        openCalendar: false,
-    };
+const CardDetailHeader = ({
+    handleOnCancel,
+    card,
+    admin,
+    classes,
+    markPrivateLessonUsed,
+    handleOnSave,
+}) => {
+    const [id, setId] = useState('');
+    const [expirationDate, setExpirationDate] = useState(new Date());
+    const [value, setValue] = useState(5);
+    const [canSave, setCanSave] = useState(false);
+    const [privateLessonLength, setPrivateLessonLength] = useState(false);
+    const [privateLessonUseDate, setPrivateLessonUseDate] = useState(
+        new Date()
+    );
+    const [openCalendar, setOpenCalendar] = useState(false);
 
-    componentDidMount() {
-        const { card } = this.props;
+    const populateHeaderWithCardData = () => {
         if (card) {
             const { id, expirationDate, value, privateLessonLength } = card;
-            this.setState({
-                id,
-                expirationDate,
-                value,
-                privateLessonLength,
-            });
+            setId(id);
+            setExpirationDate(expirationDate);
+            setValue(value);
+            setPrivateLessonLength(privateLessonLength);
         }
-    }
-
-    handleChange = (name, isNumber = false) => event => {
-        let value = event.target.value;
-        if (isNumber) {
-            value = toNumber(value);
-        }
-        this.setState({ [name]: value, canSave: true });
     };
 
-    handleOpenCalendar = () => {
-        this.setState({
-            openCalendar: true,
-        });
-    };
+    useEffect(populateHeaderWithCardData, []);
 
-    handleCloseCalendar = () => {
-        this.setState({
-            openCalendar: false,
-        });
-    };
-
-    handleSetCalendarDate = date => {
-        this.setState({
-            privateLessonUseDate: date,
-        });
-    };
-
-    handleOnMarkPrivateLessonUsed = (id, privateLessonUseDate) => {
-        this.props.markPrivateLessonUsed({
+    const handleOnMarkPrivateLessonUsed = (id, privateLessonUseDate) => {
+        markPrivateLessonUsed({
             variables: {
                 id,
                 privateLessonUseDate,
             },
         });
-        this.setState({
-            openCalendar: false,
-        });
+        setOpenCalendar(false);
     };
 
-    handleSave = () => {
-        const { id, expirationDate, value, privateLessonLength } = this.state;
-        this.props.handleOnSave({
+    const handleSave = () => {
+        handleOnSave({
             id,
             expirationDate,
             value,
             privateLessonLength,
         });
-        this.setState({
-            canSave: false,
-        });
+        setCanSave(false);
     };
 
-    handleSetExpirationDate = expirationDate => {
-        this.setState({
-            expirationDate,
-            canSave: true,
-        });
-    };
-
-    renderForm = () => {
-        const { classes, card, admin } = this.props;
-        const { value, expirationDate, privateLessonLength } = this.state;
-        return (
-            <form>
+    const renderForm = () => (
+        <form>
+            <TextField
+                id="standard-select-studio-native"
+                disabled
+                label="Student"
+                value={card.student.name}
+                className={classes.textField}
+                margin="normal"
+            />
+            <TextField
+                id="filled-number"
+                label="Value"
+                value={value}
+                onChange={event => {
+                    setValue(toNumber(event.target.value));
+                    setCanSave(true);
+                }}
+                type="number"
+                className={classes.textField}
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                margin="normal"
+                disabled={!admin}
+            />
+            <TextField
+                id="filled-number"
+                label="Private Lesson Length"
+                value={privateLessonLength}
+                onChange={event => {
+                    setValue(toNumber(event.target.value));
+                    setCanSave(true);
+                }}
+                type="number"
+                className={classes.textField}
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                margin="normal"
+                disabled={card.privateLessonUseDate || !admin}
+            />
+            {card.payment ? (
                 <TextField
                     id="standard-select-studio-native"
                     disabled
-                    label="Student"
-                    value={card.student.name}
+                    label="Payment Date"
+                    value={card.payment.date}
                     className={classes.textField}
                     margin="normal"
                 />
+            ) : null}
+            {card.payment ? (
                 <TextField
-                    id="filled-number"
-                    label="Value"
-                    value={value}
-                    onChange={this.handleChange('value', true)}
-                    type="number"
+                    id="standard-select-studio-native"
+                    disabled
+                    label="Payment Amount"
+                    value={card.payment.amount}
                     className={classes.textField}
-                    InputLabelProps={{
-                        shrink: true,
+                    margin="normal"
+                />
+            ) : null}
+            <DatePicker
+                margin="normal"
+                label="Expiration Date"
+                value={expirationDate}
+                className={classes.textField}
+                onChange={expirationDate => {
+                    setExpirationDate(expirationDate);
+                    setCanSave(true);
+                }}
+                disabled={!admin}
+            />
+        </form>
+    );
+
+    const renderPrivateCalendarDialog = () => (
+        <Dialog
+            open={openCalendar}
+            onClose={() => setOpenCalendar(false)}
+            aria-labelledby="form-dialog-title"
+        >
+            <DialogTitle id="form-dialog-title">
+                Select date private was used
+            </DialogTitle>
+            <DialogContent>
+                <Calendar
+                    date={privateLessonUseDate}
+                    onChange={date => {
+                        setPrivateLessonUseDate(date);
                     }}
-                    margin="normal"
-                    disabled={!admin}
                 />
-                <TextField
-                    id="filled-number"
-                    label="Private Lesson Length"
-                    value={privateLessonLength}
-                    onChange={this.handleChange('privateLessonLength', true)}
-                    type="number"
-                    className={classes.textField}
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    margin="normal"
-                    disabled={card.privateLessonUseDate || !admin}
-                />
-                {card.payment ? (
-                    <TextField
-                        id="standard-select-studio-native"
-                        disabled
-                        label="Payment Date"
-                        value={card.payment.date}
-                        className={classes.textField}
-                        margin="normal"
-                    />
-                ) : null}
-                {card.payment ? (
-                    <TextField
-                        id="standard-select-studio-native"
-                        disabled
-                        label="Payment Amount"
-                        value={card.payment.amount}
-                        className={classes.textField}
-                        margin="normal"
-                    />
-                ) : null}
-                <DatePicker
-                    margin="normal"
-                    label="Expiration Date"
-                    value={expirationDate}
-                    className={classes.textField}
-                    onChange={this.handleSetExpirationDate}
-                    disabled={!admin}
-                />
-            </form>
-        );
-    };
-
-    render() {
-        const { handleOnCancel, card, admin, classes } = this.props;
-
-        const {
-            canSave,
-            privateLessonLength,
-            privateLessonUseDate,
-            openCalendar,
-        } = this.state;
-
-        return (
-            <DetailHeader renderForm={this.renderForm} formOnly={!admin}>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    disabled={!canSave}
-                    onClick={this.handleSave}
-                    className={classes.button}
-                >
-                    Save
-                </Button>
-                <Button
-                    variant="contained"
-                    onClick={handleOnCancel}
-                    className={classes.button}
-                >
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={() => setOpenCalendar(false)} color="primary">
                     Cancel
                 </Button>
-                {admin ? (
-                    <Button
-                        variant="contained"
-                        onClick={this.handleOpenCalendar}
-                        className={classes.button}
-                        disabled={
-                            privateLessonLength === 0 ||
-                            card.privateLessonUseDate
-                        }
-                    >
-                        Use Private
-                    </Button>
-                ) : null}
-                <Dialog
-                    open={openCalendar}
-                    onClose={this.handleCloseCalendar}
-                    aria-labelledby="form-dialog-title"
+                <Button
+                    onClick={() =>
+                        handleOnMarkPrivateLessonUsed(
+                            card.id,
+                            privateLessonUseDate
+                        )
+                    }
+                    color="primary"
                 >
-                    <DialogTitle id="form-dialog-title">
-                        Select date private was used
-                    </DialogTitle>
-                    <DialogContent>
-                        <Calendar
-                            date={privateLessonUseDate}
-                            onChange={this.handleSetCalendarDate}
-                        />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button
-                            onClick={this.handleCloseCalendar}
-                            color="primary"
-                        >
-                            Cancel
-                        </Button>
-                        <Button
-                            onClick={() =>
-                                this.handleOnMarkPrivateLessonUsed(
-                                    card.id,
-                                    privateLessonUseDate
-                                )
-                            }
-                            color="primary"
-                        >
-                            Log Private Lesson Used
-                        </Button>
-                    </DialogActions>
-                </Dialog>
-            </DetailHeader>
-        );
-    }
-}
+                    Log Private Lesson Used
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+
+    return (
+        <DetailHeader renderForm={renderForm} formOnly={!admin}>
+            <Button
+                variant="contained"
+                color="primary"
+                disabled={!canSave}
+                onClick={handleSave}
+                className={classes.button}
+            >
+                Save
+            </Button>
+            <Button
+                variant="contained"
+                onClick={handleOnCancel}
+                className={classes.button}
+            >
+                Cancel
+            </Button>
+            {admin ? (
+                <Button
+                    variant="contained"
+                    onClick={() => setOpenCalendar(true)}
+                    className={classes.button}
+                    disabled={
+                        privateLessonLength === 0 || card.privateLessonUseDate
+                    }
+                >
+                    Use Private
+                </Button>
+            ) : null}
+            {renderPrivateCalendarDialog()}
+        </DetailHeader>
+    );
+};
 
 CardDetailHeader.propTypes = {
     classes: PropTypes.object.isRequired,
